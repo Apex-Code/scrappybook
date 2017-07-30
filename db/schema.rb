921e7b02acc6714833a18adf049449b6b6905969
Apex-Code <upstaterounder@gmail.com>
2017-07-29 22:12:02 -0500

    add email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730025625) do
+ActiveRecord::Schema.define(version: 20170730031113) do
 
   create_table "scraps", force: :cascade do |t|
     t.string "title"
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(version: 20170730025625) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index [nil], name: "index_users_on_email", unique: true
   end
 
 end
