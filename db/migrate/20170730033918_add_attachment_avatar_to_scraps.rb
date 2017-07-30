1adb11edcd8eb5ee44bcd400ea047842693538ff
Apex-Code <upstaterounder@gmail.com>
2017-07-29 23:24:28 -0500

    update migration, migrate db

--- db/migrate/20170730033918_add_attachment_avatar_to_scraps.rb
+++ db/migrate/20170730033918_add_attachment_avatar_to_scraps.rb
@@ -1,4 +1,4 @@
-class AddAttachmentAvatarToScraps < ActiveRecord::Migration
+class AddAttachmentAvatarToScraps < ActiveRecord::Migration[5.1]
   def self.up
     change_table :scraps do |t|
       t.attachment :avatar
