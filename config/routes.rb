fef955d4dae4745e9fdf75d5eea6d8d7d5985f4e
Apex-Code <upstaterounder@gmail.com>
2017-07-29 20:22:22 -0500

    add resources scraps

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  get 'scraps/index'
-
-  get 'scraps/edit'
-
-  get 'scraps/update'
-
-  get 'scraps/show'
-
-  get 'scraps/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :scraps
 end
